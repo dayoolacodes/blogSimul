13abec36a3a7fde59d0b5c2318ad1e1297f8a353
dayoola <ayodayo1@gmail.com>
2020-09-11 19:11:52 +0100

    adjusted axios instance

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "http://jsonplaceholder.typicode.com"
-})
+    baseURL: 'http://jsonplaceholder.typicode.com'
+});
  
-instance.defaults.common['Authorization'] = 'Auth Token from axios-instance'
+instance.defaults.headers.common['Authorization'] = 'Auth Token from axios-instance'
 
 export default instance;
